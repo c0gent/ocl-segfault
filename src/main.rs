88c5649ecb759c26b1696bcbf50e7d55ba7dc0e8
nsan1129 <nsan1129@gmail.com>
2018-06-03 14:31:38 -0700

    Decompose `ProQue`.

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,43 @@
 extern crate ocl;
 
 use std::thread;
-use ocl::{Buffer, Context, Device, Kernel, Platform, ProQue, Program, MemFlags, Queue};
+use ocl::{Buffer, Context, Device, Kernel, Platform, ProQue, Program, MemFlags, Queue,
+    Error as OclError};
 use ocl::builders::DeviceSpecifier;
 
 const DRAGON_SRC: &'static str = include_str!("sequential.cl");
 const TH_C: usize = 30;
-const LOOP_C: usize = 1000;
+const LOOP_C: usize = 500;
 
 fn main() {
     let mut threads = Vec::with_capacity(TH_C);
 
     for th_i in 0..TH_C {
         threads.push(thread::spawn(move || {
-            println!("Thread {}: Platform: {}", th_i, Platform::default().name().unwrap());
+
+            let platform = Platform::default();
+            let device = Device::first(&platform).unwrap();
+            let context = Context::builder()
+                .platform(platform.clone())
+                .devices(device.clone())
+                .build().unwrap();
+            // let _program = Program::builder()
+            //     .devices(device.clone())
+            //     .src(DRAGON_SRC)
+            //     .build(&context).unwrap();
+            // let _queue = Queue::new(&context, device, None).unwrap();
+
+            println!("Thread {}: Platform: {}", th_i, platform.name().unwrap());
             for _ in 0..LOOP_C {
-                let _ = ProQue::builder()
+                // let context = Context::builder()
+                //     .platform(platform.clone())
+                //     .devices(device.clone())
+                //     .build().unwrap();
+                let _program = Program::builder()
+                    .devices(device.clone())
                     .src(DRAGON_SRC)
-                    .build().unwrap();
+                    .build(&context).unwrap();
+                let _queue = Queue::new(&context, device, None).unwrap();
             }
         }));
     }
